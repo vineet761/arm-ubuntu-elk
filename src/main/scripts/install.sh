945f1f12fa80dfc8f1cb4a2475985b3e8d5d19de
vineet761 <vineet761@gmail.com>
2020-08-06 01:47:41 +0530

    Update install.sh

--- src/main/scripts/install.sh
+++ src/main/scripts/install.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get install openjdk-8-jre-headless -y
 # Install ELK
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 apt-get install apt-transport-https -y
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-6.x.list
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
 apt-get update && apt-get install elasticsearch logstash kibana -y
 
 # Configure and start ElasticSearch
